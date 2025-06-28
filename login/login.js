d1da2b54c51f50297a500a97b522414b5b3ea518
Miranda Honorato <112159202+miaeo@users.noreply.github.com>
2025-06-27 21:04:42 -0300

    Update login.js

--- login/login.js
+++ login/login.js
@@ -58,6 +58,7 @@ btnLoginFinal.addEventListener("click", async () => {
         alert(`Seu acesso foi bloqueado.\nMotivo: ${data.motivo}`);
       }, 2000);
     } else {
+      // Outros erros de login (401 etc)
       setTimeout(() => {
         carregando.classList.add("hidden");
         alert("Email ou senha inválidos.");
@@ -103,4 +104,76 @@ registerForm.addEventListener("submit", async (e) => {
     alert("Erro ao conectar com o servidor.");
     console.error(error);
   }
+});
+
+
+// ESQUECI MINHA SENHA
+// Etapa 1 - Confirmar email
+document.querySelector('.link-senha').addEventListener('click', () => {
+  document.getElementById('modalEmail').classList.remove('hidden');
+});
+
+function fecharModalEmail() {
+  document.getElementById('modalEmail').classList.add('hidden');
+}
+function fecharModalSenha() {
+  document.getElementById('modalNovaSenha').classList.add('hidden');
+}
+
+document.getElementById('proximoPasso').addEventListener('click', () => {
+  const email = document.getElementById('emailRecuperar').value;
+  const carregando = document.getElementById('carregandoRecuperacao');
+
+  if (!email) return alert("Digite o email!");
+
+  carregando.classList.remove("hidden");
+
+  fetch('http://localhost:3000/verificar-email', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email })
+  })
+  .then(res => res.json())
+  .then(data => {
+    setTimeout(() => {
+      carregando.classList.add("hidden");
+
+      if (data.existe) {
+        document.getElementById('modalEmail').classList.add('hidden');
+        document.getElementById('modalNovaSenha').classList.remove('hidden');
+        localStorage.setItem('emailRecuperacao', email);
+      } else {
+        alert("Email não encontrado no sistema.");
+      }
+    }, 2000);
+  })
+  .catch(() => {
+    setTimeout(() => {
+      carregando.classList.add("hidden");
+      alert("Erro ao verificar email.");
+    }, 2000);
+  });
+});
+
+// Etapa 2 - Redefinir senha
+document.getElementById('confirmarRecuperacao').addEventListener('click', () => {
+  const novaSenha = document.getElementById('novaSenha').value;
+  const email = localStorage.getItem('emailRecuperacao');
+
+  if (!novaSenha) {
+    alert("Digite uma nova senha!");
+    return;
+  }
+
+  fetch('http://localhost:3000/redefinir-senha', {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, novaSenha })
+  })
+  .then(res => res.json())
+  .then(data => {
+    alert("Senha redefinida com sucesso!");
+    fecharModalSenha();
+  })
+  .catch(() => alert("Erro ao redefinir senha."));
 });
